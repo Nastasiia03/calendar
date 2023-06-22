70db9a98ce3b4f4bdb1f615a0e8c006021a78c1b
Nastasiia03 <abernatskao@gmail.com>
2023-06-23 00:47:54 +0300

    calendar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import DatePicker from "react-multi-date-picker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
+
+const weekDays = ["S", "M", "T", "W", "T", "F", "S"]
+
 export const App = () => {
+  const [startDate, setStartDate] = useState(new Date());
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <DatePicker value={startDate}
+      onChange={(date) => setStartDate(date)}
+      className="custom-calendar"
+      weekStartDayIndex={1}
+      arrow={false}
+      weekDays={weekDays}
+   mapDays={({ date }) => {
+    let props = {}
+    let isWeekend = [0, 6].includes(date.weekDay.index)
+    
+    if (isWeekend) props.className = "highlight"
+    
+    return props
+  }}
+/>
   );
-};
+}
